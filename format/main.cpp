d1d565a82db71e25dcab222323d43cdc21dfc64b
Luis Ayuso <luis@lucifer.local>
2015-02-16 21:17:25 +0100

    Initial Commit

---
+++ format/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  format
+//
+//  Created by Luis Ayuso on 16/02/15.
+//  Copyright (c) 2015 Luis Ayuso. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
